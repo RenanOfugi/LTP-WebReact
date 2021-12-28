bd570b688ffc7d7d8137cfdac8e4c3d0ee70a51e
RenanOfugi <renan.ofugi@gmail.com>
2021-12-27 23:57:29 -0300

    feat: add exercices 1 and 2

---
+++ ex1_2.js
@@ -0,0 +1,11 @@
+const idade = 18
+
+if (idade < 12) {
+    console.log("Criança");
+} else if (idade >= 12 && idade < 18) {
+    console.log("Adolescente");
+} else if (idade >= 18 && idade < 60) {
+    console.log("Adulto");
+} else {
+    console.log("Idoso");
+}
