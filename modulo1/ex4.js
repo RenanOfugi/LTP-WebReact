5842b9cce2d017e71884cad6a7d77ed70349df18
RenanOfugi <renan.ofugi@gmail.com>
2021-12-30 11:47:58 -0300

    feat: add exercicio 4 - modulo 1

---
+++ modulo1/ex4.js
@@ -0,0 +1,11 @@
+
+/*
+* Escreva um programa que imprime a soma dos 100 primeiros números
+naturais.
+*/
+
+//utilização da regra de PA (Progressão Aritmética)
+
+const soma = ((1 + 100)*100)/2
+
+console.log("Somatório dos 100 primeiros numeros é = " + soma);
