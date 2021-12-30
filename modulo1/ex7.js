885ce4be7d568fcff0592b2192db2628bac83b76
RenanOfugi <renan.ofugi@gmail.com>
2021-12-30 14:42:42 -0300

    feat: add exercicio 7 - modulo 1

---
+++ modulo1/ex7.js
@@ -0,0 +1,8 @@
+/*
+ * Escreva uma função que recebe o valor do raio de um círculo e retorne o seu
+perímetro (C = 2*pi*r)
+*/
+
+const perimetro = (raio) => 2 * Math.PI * raio;
+
+console.log(perimetro(5))
